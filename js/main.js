ad82b399c471b23bcc7de726abfd985595b41f35
Alexander Yaroslavtsev <a1exweb@mail.ru>
2020-01-27 17:39:01 +0300

    feat: add structure section projects

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 40) {
-        $('.fixed').addClass('fixed--active');
-    } else {
-        $('.fixed').removeClass('fixed--active');
-    }
-});
+// $(window).scroll(function () {
+//     if ($(this).scrollTop() > 40) {
+//         $('.fixed').addClass('fixed--active');
+//     } else {
+//         $('.fixed').removeClass('fixed--active');
+//     }
+// });
 
 $(document).ready(function() {
 	$('.fullpage').fullpage({
         scrollOverflow: true
-	});
+    });
 });
