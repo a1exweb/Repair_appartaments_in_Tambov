5b2fcb3e5b8d9cb26b5eb3a97d619997df2330e3
Alexander Yaroslavtsev <a1exweb@mail.ru>
2020-01-31 20:05:41 +0300

    feat: add section calculator

--- js/main.js
+++ js/main.js
@@ -7,23 +7,35 @@
 // });
 
 $(document).ready(function() {
-	// $('.fullpage').fullpage({
-    //     scrollOverflow: true,
-    //     scrollOverflowOptions: {click: false},
-    //     afterRender: function () {
-    //         $('.projects__last').html($('.projects__slides').length);
-    //     },
-    //     afterSlideLoad: function (section, origin, destination) {
-    //         $('.projects__first').html(++destination.index);
-    //     }
-    // });
+	$('.fullpage').fullpage({
+        scrollOverflow: true,
+        scrollOverflowOptions: {click: false},
+        afterRender: function () {
+            $('.projects__last').html($('.projects__slides').length);
+        },
+        afterSlideLoad: function (section, origin, destination) {
+            $('.projects__first').html(++destination.index);
+        }
+    });
 
     $(document).on('click', '.projects__mini a', changeImg);
+    $(document).on('input', '.calc', changeInput);
 
     function changeImg(event) {
         event.preventDefault();
         $('.projects__big img').attr('src', $(this).attr('data-src'));
     }
+    let price = 16000;
+    function changeInput(event) {
+        event.preventDefault();
+        $(this).find('.calc__range output').val($(this).find('.calc__range input').val());
+        // $(this).find('.calc__result span').html($(this).find('.calc__range input').val()*price);
+    }
+
+    $('.calc__button').on('click', function (event) {
+        event.preventDefault();
+        $('.calc').find('.calc__result span').html($('.calc').find('.calc__range input').val()*price);
+    });
 
     let modalOverlay = $('.modal-overlay'),
         modalForm = $('.modal-form');
