b0f88ac6e3b3907bd2db2d05fa2a9d846fa92d92
Alexander Yaroslavtsev <a1exweb@mail.ru>
2020-02-03 15:35:25 +0300

    fixed: small details (logo, menu)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-// $(window).scroll(function () {
-//     if ($(this).scrollTop() > 40) {
-//         $('.fixed').addClass('fixed--active');
-//     } else {
-//         $('.fixed').removeClass('fixed--active');
-//     }
-// });
-
 $(document).ready(function() {
 	$('.fullpage').fullpage({
         scrollOverflow: true,
